59e9b6a6ac3ca4f5f2b46f7611255d5147600866
ViverMan <blackpolitik983@gmail.com>
2024-09-25 10:38:04 +0300

    с http на https
    с http на https

--- docs/js/index.bundle.js
+++ docs/js/index.bundle.js
@@ -115,7 +115,7 @@ function weather() {
     }
 
     async function getGeo(name) {
-        const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=2&appid=${API_KEY}`;
+        const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=2&appid=${API_KEY}`;
 
         // let geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${name},RU&limit=5&appid=${API_KEY}`;
 
